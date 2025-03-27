ed44a22129977e91a4eb00a96c7d4f7aca2a191f
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-27 21:34:38 +0800

    Update check_string_ends_with_alternative.py
    This will be the Final revision of the program number 5 (Clean version)

--- check_string_ends_with_alternative.py
+++ check_string_ends_with_alternative.py
@@ -1,15 +1,9 @@
 # Program: Check if String Ends with Given Suffix
 
-# Get input from the user for the main string
 input_string = input("Enter the main string: ")
-
-# Get input from the user for the suffix to check
 suffix = input("Enter the suffix: ")
 
-# Compare the last characters of the main string with the suffix
 if input_string[-len(suffix):] == suffix:
-    # If they match, print "Ends with given suffix"
     print("Ends with given suffix")
 else:
-    # Otherwise, print "Does not end with given suffix"
     print("Does not end with given suffix")
