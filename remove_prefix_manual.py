6e4d0ab88e9d03e81f2e1a4323c260560d6c735c
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-27 20:26:49 +0800

    Update remove_prefix_manual.py
    Ask the user to enter a string

--- remove_prefix_manual.py
+++ remove_prefix_manual.py
@@ -1,6 +1,8 @@
 # Program: Remove a prefix manually without using removeprefix()
 
-# Step 1: Ask the user to enter a string
+# Ask the user to enter a string
+input_string = input("Enter a string: ")
+
 # Step 2: Ask the user to enter a prefix to remove
 # Step 3: Check if the string starts with the given prefix
 # Step 4: If it does, remove the prefix by slicing
