b3f06ea68c00fc16ed6baa9ea52897c437ba93c0
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-27 22:42:46 +0800

    Update alternative_capitalization.py
    combine the first letter and remaining letters, print empty string if the user didn't input string...

--- alternative_capitalization.py
+++ alternative_capitalization.py
@@ -11,4 +11,7 @@ if input_string:
 # Convert the rest of the string to lowercase
     remaining_letters = input_string[1:].lower()
 
-# Step 5: Combine both parts and display the result.
+    # Combine and print the result
+    print(first_letter + remaining_letters)
+else:
+    print("Empty string.")
