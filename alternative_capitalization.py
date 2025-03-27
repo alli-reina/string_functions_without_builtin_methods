ad497b9976853df4975952fc94d17e4590c1648f
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-27 22:51:50 +0800

    Update alternative_capitalization.py
    This is the Final rwsvion, this is a cleaner version.

--- alternative_capitalization.py
+++ alternative_capitalization.py
@@ -1,17 +1,10 @@
 # Program: Manually Capitalize First Letter of a String
 
-# Get input from the user
 input_string = input("Enter a string: ")
 
-# Check if the string is not empty
-if input_string:
-    # Convert the first letter to uppercase
-    first_letter = input_string[0].upper().
-
-# Convert the rest of the string to lowercase
-    remaining_letters = input_string[1:].lower()
-
-    # Combine and print the result
-    print(first_letter + remaining_letters)
-else:
+if not input_string:
     print("Empty string.")
+else:
+    first_letter = input_string[0].upper()
+    remaining_letters = input_string[1:].lower()
+    print(first_letter + remaining_letters)
