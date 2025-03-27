fcdc31fa27d758507b389f798e78c126f2faad02
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-27 20:52:39 +0800

    Update convert_to_lowercase_alternative.py
    Print the result (Final string).

--- convert_to_lowercase_alternative.py
+++ convert_to_lowercase_alternative.py
@@ -3,16 +3,17 @@
 # Get input from the user
 input_string = input("Enter a string: ")
 
-# Create an empty result string
+# Step 2: Create an empty string for the result
 result = ""
- 
+
 # Go through each letter in the input
 for letter in input_string:
-# If the letter is uppercase, replace it with lowercase
- if "A" <= letter <= "Z":
-  letter(chr(ord(letter) +32)
+    # If it's uppercase, convert to lowercase
+    if "A" <= letter <= "Z":
+        letter = chr(ord(letter) + 32)
 
-# Add it to the result 
-  result += letter
+    # Add it to the result
+    result += letter
 
-# Step 6: Print the final result
+# Print the final result
+print("Final string:", result)
