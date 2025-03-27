456ba86f3cf1c6656b53e295a78f8f0122bc55d4
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-27 20:12:13 +0800

    Update remove_leading_spaces.py
    Extracting the substring starting from the first non-space character, and then display the result

--- remove_leading_spaces.py
+++ remove_leading_spaces.py
@@ -6,9 +6,12 @@ user_input = input("Enter a string: ")
 # Initialize index to track the first non-space character
 index = 0
 
-# Step 3: Loop until a non-space character is found
+# Loop until a non-space character is found
 while index < len(user_input) and user_input[index] == " ":
     index += 1
 
-# Step 4: Extract the substring starting from the first non-space character
-# Step 5: Display the result
+# Extract the substring starting from the first non-space character
+result = user_input[index:]
+
+# Display the result
+print(result)
