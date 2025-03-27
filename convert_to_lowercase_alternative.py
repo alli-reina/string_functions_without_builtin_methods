d70fabfdf7fa53b073ab1ba2f75c883f11a6ab52
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-27 20:46:47 +0800

    Create convert_to_lowercase_alternative.py
    This contains the Pseduocode needed to convert without using the built in code .lower()

---
+++ convert_to_lowercase_alternative.py
@@ -0,0 +1,8 @@
+# Program: Convert all uppercase letters to lowercase without using lower()
+
+# Step 1: Get input from the user  
+# Step 2: Create an empty result string  
+# Step 3: Go through each letter in the input  
+# Step 4: If the letter is uppercase, replace it with lowercase  
+# Step 5: Add it to the result  
+# Step 6: Print the final result
