7f536e5b6d322c14fc2b9ef32de1cbcc8ef0d8a8
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-27 22:15:50 +0800

    Update alternative_manual_ljust.py
    This will be the final revision for now, clean version.

--- alternative_manual_ljust.py
+++ alternative_manual_ljust.py
@@ -1,18 +1,9 @@
 # Program: Manual Implementation of ljust()
 
-# Get input from the user
 input_string = input("Enter a string: ")
-
-# Get the desired width from the user
 width = int(input("Enter the desired width: "))
 
-# Check if the string length is less than the desired width
 if len(input_string) < width:
-    # Calculate how many spaces to add
-    spaces_to_add = width - len(input_string)
+    input_string += " " * (width - len(input_string))
 
-    # Append spaces to the right
-    input_string += " " * spaces_to_add  
-
-# Print the result
 print(f"'{input_string}'")
