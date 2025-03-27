9540334b19c24f3db194ec188ee410dcfb089804
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-27 20:48:10 +0800

    Update convert_to_lowercase_alternative.py
    Create an empty result string

--- convert_to_lowercase_alternative.py
+++ convert_to_lowercase_alternative.py
@@ -3,7 +3,9 @@
 # Get input from the user
 input_string = input("Enter a string: ")
 
-# Step 2: Create an empty result string  
+# Create an empty result string
+result = ""
+ 
 # Step 3: Go through each letter in the input  
 # Step 4: If the letter is uppercase, replace it with lowercase  
 # Step 5: Add it to the result  
