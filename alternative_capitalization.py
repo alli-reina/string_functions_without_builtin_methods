a1de568b2a50f3622a8ebdddac6be486cbf08c41
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-27 22:38:56 +0800

    Create alternative_capitalization.py
    the pseudocode code needed

---
+++ alternative_capitalization.py
@@ -0,0 +1,7 @@
+# Program: Manually Capitalize First Letter of a String
+
+# Step 1: Ask the user to enter a string.
+# Step 2: Check if the string is not empty and if it's empty, print a message and stop.
+# Step 3: Convert the first letter to uppercase.
+# Step 4: Convert the rest of the string to lowercase.
+# Step 5: Combine both parts and display the result.
