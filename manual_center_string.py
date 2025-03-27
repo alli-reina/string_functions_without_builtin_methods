1c965380775e6fa73cae9dd5cbe463c94ca2ae62
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-27 22:34:13 +0800

    Update manual_center_string.py
    This will be the final revision for now, this is the more clean version.

--- manual_center_string.py
+++ manual_center_string.py
@@ -1,19 +1,10 @@
 # Program: Center a string manually without using center()
 
-# Get input from the user (string and total width)
 input_string = input("Enter a string: ")
 total_width = int(input("Enter the total width: "))
 
-# Check if total width is too small
 if total_width <= len(input_string):
     print("'" + input_string + "'")
-
 else:
-    # Compute the number of spaces needed
-    spaces = (total_width - len(input_string)) // 2  
-
-    # Add spaces before and after
-    centered_string = " " * spaces + input_string + " " * spaces  
-
-    # Print the result wrapped in single quotes
-    print("'" + centered_string + "'")
+    spaces = (total_width - len(input_string)) // 2
+    print("'" + " " * spaces + input_string + " " * spaces + "'")
