c86e062d5a6ffe3e73f819f8958353640a0efd9b
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-27 22:06:46 +0800

    Update alternative_manual_ljust.py
    Get input from the user.

--- alternative_manual_ljust.py
+++ alternative_manual_ljust.py
@@ -1,6 +1,8 @@
 # Program: Manual Implementation of ljust()
 
-# Step 1: Get input from the user
+# Get input from the user
+input_string = input("Enter a string: ")
+
 # Step 2: Get the desired width from the user
 # Step 3: Check if the string length is less than the desired width
 # Step 4: If the string is shorter, calculate how many spaces to add
