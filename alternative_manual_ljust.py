75dcb273c7f6e9510ee8d26e608d306fb04d3ef9
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-27 22:07:28 +0800

    Update alternative_manual_ljust.py
    this will now get the desired width from the user.

--- alternative_manual_ljust.py
+++ alternative_manual_ljust.py
@@ -3,7 +3,9 @@
 # Get input from the user
 input_string = input("Enter a string: ")
 
-# Step 2: Get the desired width from the user
+# Get the desired width from the user
+width = int(input("Enter the desired width: "))
+
 # Step 3: Check if the string length is less than the desired width
 # Step 4: If the string is shorter, calculate how many spaces to add
 # Step 5: Append the spaces to the right of the string
