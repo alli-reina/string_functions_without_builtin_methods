4e5d84034db336e40757b2a933567ee00b2d00da
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-27 21:06:12 +0800

    Update check_if_uppercase_alternative.py
    This will be the final revision, this is the more clean version of the code.

--- check_if_uppercase_alternative.py
+++ check_if_uppercase_alternative.py
@@ -1,18 +1,13 @@
 # Program: Check if all characters are uppercase without using isupper()
 
-# Get input from the user
 input_string = input("Enter a string: ")
-
-# Check if all characters are uppercase
 all_upper = True  
 
 for letter in input_string:
-    # If a lowercase letter is found, set all_upper to False
     if "a" <= letter <= "z":
         all_upper = False
         break  
 
-# Print the result
 if all_upper:
     print("All uppercase")
 else:
