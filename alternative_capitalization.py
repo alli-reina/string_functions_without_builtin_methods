1ecdde97f59c362d01a2cd557d17807965b7a2cd
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-27 22:40:07 +0800

    Update alternative_capitalization.py
    get the input from the user.

--- alternative_capitalization.py
+++ alternative_capitalization.py
@@ -1,6 +1,8 @@
 # Program: Manually Capitalize First Letter of a String
 
-# Step 1: Ask the user to enter a string.
+# Get input from the user
+input_string = input("Enter a string: ")
+
 # Step 2: Check if the string is not empty and if it's empty, print a message and stop.
 # Step 3: Convert the first letter to uppercase.
 # Step 4: Convert the rest of the string to lowercase.
