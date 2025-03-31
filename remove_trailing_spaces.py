97a2c3f3b6f08fd797938a58a1dc39b730082bbf
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-31 20:14:29 +0800

    Update remove_trailing_spaces.py
    Ask the user to enter a string.

--- remove_trailing_spaces.py
+++ remove_trailing_spaces.py
@@ -1,13 +1,4 @@
-BEGIN  
-    # Ask the user to enter a string  
-    PRINT "Enter a string: "  
-    INPUT text  
+# Program: Remove Trailing Spaces from a String  
 
-    # Keep checking if the last character is a space  
-    WHILE text is not empty AND last character is a space  
-        # Remove the last space  
-        REMOVE last character from text  
-
-    # Display the final string with single quotes around it  
-    PRINT "String without trailing spaces: '" + text + "'"  
-END
+# Ask the user to enter a string  
+text = input("Enter a string: ")  
