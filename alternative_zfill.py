beb922dd8bb554bbb206895b8da6abbe74562f42
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-31 22:06:42 +0800

    Update alternative_zfill.py
    Get user input, get user's desired width.

--- alternative_zfill.py
+++ alternative_zfill.py
@@ -1,7 +1,10 @@
 # Program: Zero Fill String  
 
-# Step 1: Get user input as 'text'  
-# Step 2: Get user input as 'width' (total desired width)  
+# Get user input as 'text' 
+text = input("Enter a string: ")  
+# Get user input as 'width' (total desired width)
+width = int(input("Enter total width: "))
+
 # Step 3: Check if the length of 'text' is less than 'width'  
 #         If true, add '0' characters at the beginning to match 'width'  
-# Step 4: Print the zero-filled 'text'
+# Step 4: Print the zero-filled 'text' with single quotes
