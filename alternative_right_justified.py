f2fcba08656aa84c403249e1c8a7946348e79da5
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-31 21:54:51 +0800

    Update alternative_right_justified.py
    This would be the final revision for now.

--- alternative_right_justified.py
+++ alternative_right_justified.py
@@ -1,13 +1,9 @@
 # Program: Right Justify String  
 
-# Get user input as 'text'
-text = input("Enter a string: ")
-# Get user input as 'width' (total desired width)
-width = int(input("Enter total width: "))
+text = input("Enter a string: ")  
+width = int(input("Enter total width: "))  
 
-# Check if the length of 'text' is less than 'width'
 if len(text) < width:  
-    text = ' ' * (width - len(text)) + text
-    
-# Print the right-justified 'text'
+    text = ' ' * (width - len(text)) + text  
+
 print("'" + text + "'")
