435c5f787678161c671997f5610d93d31597d705
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-31 21:49:20 +0800

    Create alternative_right_justified.py
    Pseudocode

---
+++ alternative_right_justified.py
@@ -0,0 +1,7 @@
+# Program: Right Justify String  
+
+# Step 1: Get user input as 'text'  
+# Step 2: Get user input as 'width' (total desired width)  
+# Step 3: Check if the length of 'text' is less than 'width'  
+#         If true, add spaces at the beginning to match 'width'  
+# Step 4: Print the right-justified 'text' with single quotes
