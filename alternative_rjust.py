e85416090cd5386c97a7c3b9bf153b1b930c2a5c
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-31 23:24:29 +0800

    Create alternative_rjust.py
    pseudocode

---
+++ alternative_rjust.py
@@ -0,0 +1,8 @@
+# Program: Get Last Index of Character
+
+# Step 1: Get user input as 'text' and 'char'
+# Step 2: Set 'position' to -1 (default if character is not found)
+# Step 3: Loop backwards from the last index of 'text'
+    # Step 4: If 'text[start]' equals 'char', set 'position' to 'start' and exit loop
+# Step 5: If 'position' is -1, print "No letter found"
+# Step 6: Otherwise, print 'position'
