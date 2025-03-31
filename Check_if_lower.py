eef5625502a1c0db318088258b536d483f675d56
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-31 21:27:20 +0800

    Update Check_if_lower.py
    This is more cleaner version

--- Check_if_lower.py
+++ Check_if_lower.py
@@ -1,15 +1,10 @@
 # Program: Check if String is Lowercase  
 
-# Get user input as 'text'
-text = input("Enter a string: ")
- 
-# Set 'is_lower' to True
-is_lower = True
+text = input("Enter a string: ")  
+is_lower = True  
 
-# If the character is between 'A' and 'Z', set 'is_lower' to False
 for char in text:  
     if 'A' <= char <= 'Z':
         is_lower = False  
 
-# Print 'is_lower'
 print(is_lower)
