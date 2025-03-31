da3ea4647311a6acc2090b1235156c9662872c40
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-31 20:37:04 +0800

    Update remove_suffix.py
    Check if 'text' ends with 'suffix' if true remove.

--- remove_suffix.py
+++ remove_suffix.py
@@ -6,6 +6,8 @@ text = input("Enter a string: ")
 # Ask the user to enter a suffix to remove
 suffix = input("Enter suffix to remove: ")
 
-# Step 3: Check if 'text' ends with 'suffix'  
-# Step 4: If true, remove the suffix from 'text'  
-# Step 5: Print the modified string with single quotes
+# Check if 'text' ends with 'suffix' if true remove
+if text.endswith(suffix):
+    text = text[:len(text) - len(suffix)]
+
+# Print the modified string with single quotes
