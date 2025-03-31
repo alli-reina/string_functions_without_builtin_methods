e0aaf9d0bdc850956099928363f8d0d7df2baa04
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-31 22:47:36 +0800

    Create alternative_index.py
    Pseudocode

---
+++ alternative_index.py
@@ -0,0 +1,11 @@
+# Program: Get First Index of Character  
+
+# Step 1: Get user input as 'text'  
+# Step 2: Get user input as 'char' (character to find)  
+# Step 3: Set 'position' to -1 (default if character is not found)  
+# Step 4: Set 'i' to 0 (starting index)  
+# Step 5: Loop while 'i' is less than the length of 'text' and 'position' is -1  
+#         If 'text[i]' is equal to 'char', set 'position' to 'i'  
+#         Increase 'i' by 1  
+# Step 6: If 'position' is -1, print "No letter found"  
+#         Else, print 'position'
