60b0e983949398d10d9e29fbeab857c66dcd78ed
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-31 21:51:48 +0800

    Update alternative_right_justified.py
    Check if the length of 'text' is less than desired width

--- alternative_right_justified.py
+++ alternative_right_justified.py
@@ -5,6 +5,8 @@ text = input("Enter a string: ")
 # Get user input as 'width' (total desired width)
 width = int(input("Enter total width: "))
 
-# Step 3: Check if the length of 'text' is less than 'width'  
-#         If true, add spaces at the beginning to match 'width'  
+# Check if the length of 'text' is less than 'width'
+if len(text) < width:  
+    text = ' ' * (width - len(text)) + text
+    
 # Step 4: Print the right-justified 'text' with single quotes
