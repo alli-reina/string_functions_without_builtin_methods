0954a6a56750c0135bb9f0db5494a44ba8100cb3
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-31 23:06:31 +0800

    Update alternative_index.py
    This will be the final version for now.

--- alternative_index.py
+++ alternative_index.py
@@ -1,26 +1,17 @@
-# # Program: Get First Index of Character  
+# Program: Get index of Character on a string
 
-# Get user input as 'text'  
 text = input("Enter a string: ")  
-
-# Get user input as 'char' (character to find)  
 char = input("Enter character to get index: ")  
 
-# Set 'position' to -1
 position = -1  
-
-# Set 'start' to 0 (starting index)  
 start = 0  
 
-# Loop while 'start' is less than the length of 'text' and 'position' is -1  
-while start < len(text) and position == -1:
-    if text[i] == char:  
-        position = i  
-    start += 1
- 
-# If 'position' is -1, print "No letter found"  
+while start < len(text) and position == -1:  
+    if text[start] == char:  
+        position = start  
+    start += 1  
+
 if position == -1:  
     print("No letter found")  
-# Else, print 'position'  
 else:  
     print(position)
