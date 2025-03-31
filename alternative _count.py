563e333a2120cf995330ab3dc452c6ba4072696d
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-31 22:28:33 +0800

    Update alternative _count.py
    Get the user input text and the character to count.

--- alternative _count.py
+++ alternative _count.py
@@ -1,7 +1,10 @@
 # Program: Count Occurrences in String  
 
-# Step 1: Get user input as 'text'  
-# Step 2: Get user input as 'char' (character to count)  
+# Get user input as 'text'
+text = input("Enter a string: ")    
+# Get user input as 'char' (character to count)
+char = input("Enter character to count: ")
+
 # Step 3: Set 'count' to 0  
 # Step 4: Loop through each character in 'text':  
 #         If the character matches 'char', increase 'count' by 1  
