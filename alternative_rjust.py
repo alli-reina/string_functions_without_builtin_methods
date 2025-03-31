569d5a2d747052fc71f546f6afa4d5dfd81452ea
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-31 23:29:36 +0800

    Update alternative_rjust.py
    This will be the final version.

--- alternative_rjust.py
+++ alternative_rjust.py
@@ -1,24 +1,18 @@
 # Program: Get Last Index of Character
 
-# Get user input as 'text' and 'char'
 text = input("Enter a string: ")
 char = input("Enter character to get last index: ")
 
-# Set 'position' to -1
 position = -1
-
-# Loop backwards from the last index of 'text'
 start = len(text) - 1
+
 while start >= 0:
-    # If 'text[start]' equals 'char', set 'position' to 'start' and exit loop
     if text[start] == char:
         position = start
         break
     start -= 1
 
-# If 'position' is -1, print "No letter found"
 if position == -1:
     print("No letter found")
-# Otherwise, print 'position'
 else:
     print(position)
