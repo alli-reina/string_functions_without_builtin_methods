952ea51495cea8b36dee52bcba92e45ff268888b
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-31 20:38:20 +0800

    Update remove_suffix.py
    This is the final revision (much cleaner version.)

--- remove_suffix.py
+++ remove_suffix.py
@@ -1,14 +1,9 @@
 # Program: Removing the string suffix  
 
-# Ask the user to enter a string
 text = input("Enter a string: ")
-
-# Ask the user to enter a suffix to remove
 suffix = input("Enter suffix to remove: ")
 
-# Check if 'text' ends with 'suffix' if true remove
 if text.endswith(suffix):
     text = text[:len(text) - len(suffix)]
 
-# Print the modified string with single quotes
 print("String without suffix: '" + text + "'")
