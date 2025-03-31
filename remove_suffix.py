72816f5c168b7958ef21db99e77dae4d3034c6c3
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-31 20:35:57 +0800

    Update remove_suffix.py
    ask the user to enter a suffix to remove

--- remove_suffix.py
+++ remove_suffix.py
@@ -3,7 +3,9 @@
 # Ask the user to enter a string
 text = input("Enter a string: ")
 
-# Step 2: Ask the user to enter a suffix to remove 
+# Ask the user to enter a suffix to remove
+suffix = input("Enter suffix to remove: ")
+
 # Step 3: Check if 'text' ends with 'suffix'  
 # Step 4: If true, remove the suffix from 'text'  
 # Step 5: Print the modified string with single quotes
