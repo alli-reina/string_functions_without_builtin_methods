94bbe18bd185e99076afc7fa1d72a03c9a77c22b
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-31 22:00:53 +0800

    Create alternative_zfill.py
    Pseudocode

---
+++ alternative_zfill.py
@@ -0,0 +1,7 @@
+# Program: Zero Fill String  
+
+# Step 1: Get user input as 'text'  
+# Step 2: Get user input as 'width' (total desired width)  
+# Step 3: Check if the length of 'text' is less than 'width'  
+#         If true, add '0' characters at the beginning to match 'width'  
+# Step 4: Print the zero-filled 'text'
