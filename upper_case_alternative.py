4c1696468f88839b74b95e0662e745330d93c4df
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-31 20:55:03 +0800

    Create upper_case_alternative.py
    Pseudocode

---
+++ upper_case_alternative.py
@@ -0,0 +1,8 @@
+# Program: Convert to Uppercase  
+
+# Step 1: Get user input as 'text'  
+# Step 2: Initialize an empty string 'uppercase_text'  
+# Step 3: For each character in 'text':  
+  #If it's a lowercase letter, convert to uppercase  
+# Otherwise, keep it unchanged  
+# Step 4: Print the modified string
