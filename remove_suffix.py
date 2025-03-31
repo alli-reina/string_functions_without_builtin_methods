09bdb3fe36d5962d66a52ca6c5f15a88f4a3cece
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-31 20:33:43 +0800

    Create remove_suffix.py
    The pseudocode code.

---
+++ remove_suffix.py
@@ -0,0 +1,7 @@
+# Program: Removing the string suffix  
+
+# Step 1: Ask the user to enter a string
+# Step 2: Ask the user to enter a suffix to remove 
+# Step 3: Check if 'text' ends with 'suffix'  
+# Step 4: If true, remove the suffix from 'text'  
+# Step 5: Print the modified string with single quotes
