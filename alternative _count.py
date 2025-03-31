326526c5b493e87222af05e29abb53dab1d9a3ef
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-31 22:30:45 +0800

    Update alternative _count.py
    Set count to 0

--- alternative _count.py
+++ alternative _count.py
@@ -5,7 +5,9 @@ text = input("Enter a string: ")
 # Get user input as 'char' (character to count)
 char = input("Enter character to count: ")
 
-# Step 3: Set 'count' to 0  
+# Set 'count' to 0
+count = 0
+  
 # Step 4: Loop through each character in 'text':  
 #         If the character matches 'char', increase 'count' by 1  
 # Step 5: Print 'count'
