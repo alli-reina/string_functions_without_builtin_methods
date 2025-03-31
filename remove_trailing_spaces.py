1db16fc0251d49f07bf98393bf8d03b23035830c
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-31 20:15:20 +0800

    Update remove_trailing_spaces.py
    This will keep checking if the last characters are/is a space.

--- remove_trailing_spaces.py
+++ remove_trailing_spaces.py
@@ -1,4 +1,7 @@
 # Program: Remove Trailing Spaces from a String  
 
 # Ask the user to enter a string  
-text = input("Enter a string: ")  
+text = input("Enter a string: ") 
+
+# Keep checking if the last character is a space  
+while text and text[-1] == ' ': 
