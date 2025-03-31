02266d6fac4a85ce595444acacc1c50e42b5c0dd
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-31 21:38:59 +0800

    Update check_ends_with.py
    This will be the final revision. also the clean version. I removed the unnecessary comments.

--- check_ends_with.py
+++ check_ends_with.py
@@ -1,18 +1,13 @@
 # Program: Check if String Starts with Given Text  
 
-# Get user input as 'text'
-text = input("Enter a string: ")
-  
-# Get user input as 'start' (the text to check)
-start = input("Enter the starting text to check: ")
+text = input("Enter a string: ")  
+start = input("Enter the starting text to check: ")  
 
-# Set 'is_start' to True
-is_start = True
- 
-# Loop through each character in 'start'
+is_start = True  
+
 for i in range(len(start)):  
     if i >= len(text) or text[i] != start[i]:  
         is_start = False  
-        break
+        break  
 
-# Step 5: Print 'is_start'
+print(is_start)
