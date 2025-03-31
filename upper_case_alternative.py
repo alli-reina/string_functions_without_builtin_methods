56ff02340817f872afef8430457190e9567ccc3a
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-31 21:01:19 +0800

    Update upper_case_alternative.py
    This would be the final revision.

--- upper_case_alternative.py
+++ upper_case_alternative.py
@@ -1,17 +1,12 @@
 # Program: Convert to Uppercase  
 
-# Get user input as 'text'
-text = input("Enter a string: ")
-# Initialize an empty string 'uppercase_text' 
+text = input("Enter a string: ")  
 uppercase_text = ""  
 
-# If it's a lowercase letter, convert to uppercase  
 for char in text:  
     if 'a' <= char <= 'z':  
-        uppercase_text += chr(ord(char) - 32)
-    # Otherwise, keep it unchanged
+        uppercase_text += chr(ord(char) - 32)  
     else:  
-        uppercase_text += char
-          
-# Print the modified string
+        uppercase_text += char  
+
 print("Uppercase string: '" + uppercase_text + "'")
