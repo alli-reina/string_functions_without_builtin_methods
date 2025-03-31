d600ee4dd536999db00645148a7340b3c3c6fc0e
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-31 21:50:58 +0800

    Update alternative_right_justified.py
    Get the string and the total desired width.

--- alternative_right_justified.py
+++ alternative_right_justified.py
@@ -1,7 +1,10 @@
 # Program: Right Justify String  
 
-# Step 1: Get user input as 'text'  
-# Step 2: Get user input as 'width' (total desired width)  
+# Get user input as 'text'
+text = input("Enter a string: ")
+# Get user input as 'width' (total desired width)
+width = int(input("Enter total width: "))
+
 # Step 3: Check if the length of 'text' is less than 'width'  
 #         If true, add spaces at the beginning to match 'width'  
 # Step 4: Print the right-justified 'text' with single quotes
