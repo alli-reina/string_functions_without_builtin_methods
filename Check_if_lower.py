7c9e24714b0bb0e940e19811db8a11bf374f1a1a
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-31 21:19:13 +0800

    Update Check_if_lower.py
    Set 'is_lower' to True

--- Check_if_lower.py
+++ Check_if_lower.py
@@ -3,7 +3,9 @@
 # Get user input as 'text'
 text = input("Enter a string: ")
  
-# Step 2: Set 'is_lower' to True  
+# Set 'is_lower' to True
+is_lower = True
+
 # Step 3: For each character in 'text':  
 # If the character is between 'A' and 'Z', set 'is_lower' to False  
 # Step 4: Print 'is_lower'
