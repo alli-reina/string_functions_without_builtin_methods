545b4bc2cc58c8578f1ee6653c09fdda305ad127
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-31 22:10:05 +0800

    Update alternative_zfill.py
    This would be the final revision for now. Much cleaner version:)

--- alternative_zfill.py
+++ alternative_zfill.py
@@ -1,13 +1,9 @@
 # Program: Zero Fill String  
 
-# Get user input as 'text' 
 text = input("Enter a string: ")  
-# Get user input as 'width' (total desired width)
-width = int(input("Enter total width: "))
+width = int(input("Enter total width: "))  
 
-# Check if the length of 'text' is less than 'width'  
 if len(text) < width:  
-    text = '0' * (width - len(text)) + text
+    text = '0' * (width - len(text)) + text  
 
-# Print the zero-filled 'text' with single quotes
 print("'" + text + "'")
