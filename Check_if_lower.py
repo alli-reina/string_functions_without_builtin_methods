601bc3e37608b2d544f5e4f919840a912e483b5a
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-03-31 21:17:55 +0800

    Create Check_if_lower.py
    Pseudocode

---
+++ Check_if_lower.py
@@ -0,0 +1,7 @@
+# Program: Check if String is Lowercase  
+
+# Step 1: Get user input as 'text'  
+# Step 2: Set 'is_lower' to True  
+# Step 3: For each character in 'text':  
+# If the character is between 'A' and 'Z', set 'is_lower' to False  
+# Step 4: Print 'is_lower'
